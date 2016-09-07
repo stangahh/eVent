378b70831101db9f4af4cd2ab44dc5f63057d46c
samueljim <samueljim@msn.com>
2016-09-07 15:44:13 +1000

    working with firebase

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as  firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDkYdLo3EcIYrJ45UbL3FGMtZCXrpO5C64",
+  authDomain: "ifb299-2b973.firebaseapp.com",
+  databaseURL: "https://ifb299-2b973.firebaseio.com",
+  storageBucket: "ifb299-2b973.appspot.com",
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
