97582291f5f06d84b754b3acaed6418231716ab3
samueljim <samueljim@msn.com>
2016-09-13 14:13:31 +1000

    Fixed for work localy

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,7 @@
 <?php
 	//Author: Maxwell McLeod
 	//Constants Used Globally, Despite how it appears this is good practice
-	define('DB_SERVER', 'localhost');
+	define('DB_SERVER', 'ozbot.com.au');
 	define('DB_USER', 'ifb299');
 	define('DB_PASSWORD', 'ifb299');
 	define('DB_NAME', 'ifb299_website');
