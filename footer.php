59adec99ea8832e2c2160d8b8013e56365bee113
Jesse Stanger <jesse.stanger@gmail.com>
2016-09-16 14:50:11 +1000

    Pretty new footer

--- footer.php
+++
@@ -1,8 +0,0 @@
-<!-- footer with team name -->
-<footer class="page-footer orange">
-	<div class="footer-copyright">
-  		<div class="container" href="tos.php">
-  		Made by <a class="orange-text text-lighten-3" href="tos.php">NoneOfTheAbove</a>
-  		</div>
-	</div>
-</footer>
