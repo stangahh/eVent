d5d6b751e572ae195574a2091e7e0c3832cffec8
Jesse Stanger <jesse.stanger@gmail.com>
2016-09-16 14:43:00 +1000

    Removed static footer, made it an include

--- event.php
+++ event.php
@@ -85,15 +85,7 @@
   </div>
 	</article>
 
-<!-- footer with team name -->
-  <footer class="page-footer orange">
-    <div class="footer-copyright">
-      <div class="container" href="tos.php">
-      Made by <a class="orange-text text-lighten-3" href="tos.php">NoneOfTheAbove</a>
-      </div>
-    </div>
-  </footer>
-
+<?php include 'includes/footer.php';?>
 
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
