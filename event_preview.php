c5c0d324d9c7d87710f8a621469ebb58345090dd
Jesse Stanger <jesse.stanger@gmail.com>
2016-09-23 03:53:12 +1000

    Able to retrieve session variables

--- event_preview.php
+++ event_preview.php
@@ -1,4 +1,14 @@
 <?php
+  //Author: Maxwell McLeod
+  require_once 'classes/Membership.php';
+
+  $membership = New Membership(); //simple new class call
+  $membership->confirm_member(); //checks if a user is logged in, any user! (yes this is insecure but i made it simple =)
+  $username = $membership->get_username(); //local variable of activer user username
+  $organisation_id = $membership->get_org_id($username); //get organisation id for user
+  $organisation_name = $membership->get_org_name($organisation_id); //get organisation name for user
+  $events = $membership->get_event_list(0); //fetches an array of all events and stores as local variable
+
   if (isset($_POST['submit'])) {
     $_SESSION['event_name'] = $_POST['event_name'];
     $_SESSION['event_desc'] = $_POST['event_desc'];
@@ -24,7 +34,11 @@
   <!-- New page content  -->
   <div class="row">
     <h1 class="heading center">Event Preview</h1>
+    <p><?php echo $_POST['event_name'] ?></p>
+    <p><?php echo $_POST['event_desc'] ?></p>
   </div>
+
+  <!-- Here put Submit button that actually goes to database-->
 
 <!--  Scripts-->
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
