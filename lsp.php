6359d0da6cfcf71b2b70bed4a1f0332905eca5bd
Incurable <t.deakin@live.com.au>
2016-09-23 00:36:35 +1000

    Added map to lsp.php
    - Styled
    - Geolocation works
    - Doesn't take up entire screen
    - Not sure what else it's supposed to be doing

--- lsp.php
+++ lsp.php
@@ -1,113 +1,172 @@
-<?php
-	//Author: Maxwell McLeod
-	require_once 'classes/Membership.php';
-
-	$membership = New Membership(); //simple new class call
-	$membership->confirm_member(); //checks if a user is logged in, any user! (yes this is insecure but i made it simple =)
-	$username = $membership->get_username(); //local variable of activer user username
-	$organisation_id = $membership->get_org_id($username); //get organisation id for user
-	$organisation_name = $membership->get_org_name($organisation_id); //get organisation name for user
-	$events = $membership->get_event_list(0); //fetches an array of all events and stores as local variable
-
-?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
   <title>eVent - Home</title>
 
   <!-- CSS  -->
   <link rel="shortcut icon" href="media/favicon.ico">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+
+  <style>
+    html,
+    body {
+      height: 100%;
+    }
+    
+    #map {
+      height: 400px;
+      width: 600px;
+    }
+  </style>
 </head>
+
 <body>
 
   <?php include 'includes/navigation.php' ?>
 
-<!-- home page content  -->
-<h1 class="center heading">eVents</h1>
-<style type="text/css">
-		/* Set a size for our map container, the Google Map will take up 100% of this container */
-		#map {
-				
-				height: 500px;
-		}
-</style>
+    <!-- home page content  -->
+    <h1 class="center heading">eVents</h1>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {
+            lat: -34.397,
+            lng: 150.644
+          },
+          zoom: 6,
+          styles: [{
+            "featureType": "administrative",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#444444"
+            }]
+          }, {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [{
+              "color": "#f2f2f2"
+            }]
+          }, {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers": [{
+              "visibility": "off"
+            }]
+          }, {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers": [{
+              "saturation": -100
+            }, {
+              "lightness": 45
+            }]
+          }, {
+            "featureType": "road.highway",
+            "elementType": "all",
+            "stylers": [{
+              "visibility": "simplified"
+            }]
+          }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [{
+              "color": "#edc200"
+            }]
+          }, {
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#000000"
+            }]
+          }, {
+            "featureType": "road.highway",
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+              "color": "#ffffff"
+            }, {
+              "visibility": "simplified"
+            }]
+          }, {
+            "featureType": "road.arterial",
+            "elementType": "labels.icon",
+            "stylers": [{
+              "visibility": "off"
+            }]
+          }, {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [{
+              "visibility": "off"
+            }]
+          }, {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [{
+              "color": "#f5a301"
+            }, {
+              "visibility": "on"
+            }]
+          }, {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#000000"
+            }]
+          }, {
+            "featureType": "water",
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+              "visibility": "off"
+            }]
+          }]
+        });
+        var infoWindow = new google.maps.InfoWindow({
+          map: map
+        });
 
-<div id="map"></div>
-<script src="https://maps.googleapis.com/maps/api/js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB62udX6drscmtSIrU8hGB8W6UGjP10lEY&callback=initMap"></script>
-<script>
-		// When the window has finished loading create our google map below
-		google.maps.event.addDomListener(window, 'load', init);
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
-		function init() {
-				// Basic options for a simple Google Map
-				// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-				var mapOptions = {
-						// How zoomed in you want the map to start at (always required)
-						zoom: 11,
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You are here! ');
+            map.setCenter(pos);
+          }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
 
-						// The latitude and longitude to center the map (always required)
-						center: new google.maps.LatLng(40.6700, -73.9400), // New York
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+          'Error: The Geolocation service failed.' :
+          'Error: Your browser doesn\'t support geolocation.');
+      }
+    </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD96Lw0LXZoGQTthyvcOkvsIa3FEiOmeCI&callback=initMap">
+    </script>
 
-						// How you would like to style the map.
-						styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#edc200"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#f5a301"},{"visibility":"on"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#000000"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]}]
-				};
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/init.js"></script>
 
-				// Get the HTML DOM element that will contain your map
-				// We are using a div with id="map" seen below in the <body>
-				var mapElement = document.getElementById('map');
+</body>
 
-				// Create the Google Map using our element and options defined above
-				var map = new google.maps.Map(mapElement, mapOptions);
-
-				// Let's also add a marker while we're at it
-				var marker = new google.maps.Marker({
-						position: new google.maps.LatLng(40.6700, -73.9400),
-						map: map,
-						title: 'Snazzy!'
-				});
-
-		// Try HTML5 geolocation.
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function(position) {
-			var pos = {
-				lat: position.coords.latitude,
-				lng: position.coords.longitude
-			};
-
-			mapElement.setPosition(pos);
-			mapElement.setContent('Location found.');
-			map.setCenter(pos);
-			}, function() {
-			handleLocationError(true, mapElement, map.getCenter());
-			});
-		} else {
-			// Browser doesn't support Geolocation
-			handleLocationError(false, mapElement, map.getCenter());
-		}
-		}
-
-		function handleLocationError(browserHasGeolocation, mapElement, pos) {
-		mapElement.setPosition(pos);
-		mapElement.setContent(browserHasGeolocation ?
-								'Error: The Geolocation service failed.' :
-								'Error: Your browser doesn\'t support geolocation.');
-		}
-	</script>
-
-  
-  <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
-
-  </body>
-  
-  <?php include 'includes/footer.php' ?>
+<?php include 'includes/footer.php' ?>
 
 </html>
