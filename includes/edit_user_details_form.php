1473d905421298b950ced356e0aedb720e248c40
Jesse Stanger <jesse.stanger@gmail.com>
2016-09-30 14:14:41 +1000

    Commented mysqli_error, at least it updates the database now.

--- includes/edit_user_details_form.php
+++ includes/edit_user_details_form.php
@@ -13,31 +13,31 @@
 	<!-- First Name -->
 	<div class="row">
 		<div class="input-field col s12">
-			<label for="fname" id="flabel">First Name</label>
+			<label for="fname">First Name</label>
 			<input type="text" id="fname" name="fname" value="<?php echo $GLOBALS['first_name']; ?>">
-		</div>
-	</div>
-
-	<!-- Username Name -->
-	<div class="row">
-		<div class="input-field col s12">
-			<label for="username" id="flabel">Username Name</label>
-			<input type="text" id="username" name="username" value="<?php echo $GLOBALS['username']; ?>">
 		</div>
 	</div>
 
 	<!-- Last Name -->
 	<div class="row">
 		<div class="input-field col s12">
-			<label for="lname" id="flabel">Last Name</label>
+			<label for="lname">Last Name</label>
 			<input type="text" id="lname" name="lname" value="<?php echo $GLOBALS['last_name']; ?>">
+		</div>
+	</div>
+
+	<!-- Username Name -->
+	<div class="row">
+		<div class="input-field col s12">
+			<label for="username">Username</label>
+			<input type="text" id="username" name="username" value="<?php echo $GLOBALS['username']; ?>">
 		</div>
 	</div>
 
 	<!-- Phone -->
 	<div class="row">
 		<div class="input-field col s12">
-			<label for="phone" id="flabel">Phone</label>
+			<label for="phone">Phone</label>
 			<input type="text" id="phone" name="phone" value="<?php echo $GLOBALS['phone']; ?>">
 		</div>
 	</div>
@@ -45,7 +45,7 @@
 	<!-- Address -->
 	<div class="row">
 		<div class="input-field col s12">
-			<label for="address" id="flabel">Address</label>
+			<label for="address">Address</label>
 			<input type="text" id="address" name="address" value="<?php echo $GLOBALS['address']; ?>">
 		</div>
 	</div>
@@ -53,7 +53,7 @@
 	<!-- DOB -->
 	<div class="row">
 		<div class="input-field col s12">
-			<label for="dob" id="flabel" class="datepicker">Date of Birth</label>
+			<label for="dob" class="datepicker">Date of Birth</label>
 			<input type="text" id="dob" name="dob" value="<?php echo $GLOBALS['DOB']; ?>">
 		</div>
 	</div>
@@ -72,7 +72,7 @@
 	<!-- Email -->
 	<div class="row">
 		<div class="input-field col s12">
-			<label for="email" id="flabel">Email Address</label>
+			<label for="email">Email Address</label>
 			<input type="text" id="email" name="email" value="<?php echo $GLOBALS['email']; ?>">
 		</div>
 	</div>
@@ -80,7 +80,7 @@
 	<!--Occupation -->
 	<div class="row">
 		<div class="input-field col s12">
-			<label for="occupation" id="flabel">Occupation</label>
+			<label for="occupation">Occupation</label>
 			<input type="text" id="occupation" name="occupation" value="<?php echo $GLOBALS['occupation']; ?>">
 		</div>
 	</div>
