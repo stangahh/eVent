e457a31c4a9a93cf26954a19afe0f40e1212a456
Jesse Stanger <jesse.stanger@gmail.com>
2016-09-30 18:48:00 +1000

    Put $image back into the mix
    plz push to server

--- home.php
+++ home.php
@@ -57,7 +57,7 @@
     <div class="card medium hoverable">
 			<a href="event.php?eventid=<?php echo $id; ?>">
       <div class="card-image waves-effect waves-block waves-light">
-        <img src="/eventimg/<?php echo $event_photo; ?>.jpg">
+        <img src="eventimg/<?php echo $event_photo; ?>.jpg">
       </div>
 			</a>
       <div class="card-stacked">
--- includes/edit_user_events.php
+++ includes/edit_user_events.php
@@ -17,7 +17,7 @@ $events = $membership->get_event_list($organisation_id); //fetches an array of a
 		    <div class="card medium hoverable">
 				<a href="event.php?eventid=<?php echo $id; ?>">
 		      		<div class="card-image waves-effect waves-block waves-light">
-		        		<img src="/eventimg/<?php echo $event_photo; ?>.jpg">
+		        		<img src="eventimg/<?php echo $event_photo; ?>.jpg">
 		      		</div>
 				</a>
 		      	<div class="card-stacked">
