d4e2e839080e19eb849ab7526146e79f58e0948b
Jesse Stanger <jesse.stanger@gmail.com>
2016-09-30 13:34:16 +1000

    Navigation Update

--- includes/navigation.php
+++ includes/navigation.php
@@ -4,24 +4,26 @@
     
       <!-- Items on the top nav bar in desktop mode -->
       <ul class="right hide-on-med-and-down">
-        <li><a href="home.php" class="active tooltipped" data-position="bottom" data-tooltip="What's trending">Home</a></li>
-        <li><a href="new_event.php" class="tooltipped" data-position="bottom" data-tooltip="asdf">New Event</a></li>
-        <li><a href="lsp.php" class="tooltipped" data-position="bottom" data-tooltip="Lots of stuff is on">Find Events</a></li>
-        <li><a href="accountsettings.php"><span class="name"><?php echo "My Account - " . $username ?></span></a></li>
-        <li><a href="login.php?status=logout" class="tooltipped" data-position="bottom" data-tooltip="Cya later"><?php echo "Logout - " . $username ?></a></li>
+        <li><a href="home.php" class="active tooltipped" data-position="bottom" data-tooltip="All Events">Home</a></li>
+        <li><a href="new_event.php" class="tooltipped" data-position="bottom" data-tooltip="Create a New Event here">New Event</a></li>
+        <li><a href="lsp.php" class="tooltipped" data-position="bottom" data-tooltip="Find all the nearest events to you">Find Events</a></li>
+        <li><a href="accountsettings.php" class="tooltipped" data-position="bottom" data-tooltip="Change your account details"><span class="name">My Account</span></a></li>
+        <li><a href="login.php?status=logout" class="tooltipped" data-position="bottom" data-tooltip="Goodbye!"><?php echo "Logout - " . $username ?></a></li>
       </ul>
 
       <!-- Code for the sidenav -->
-        <ul id="nav-mobile" class="side-nav">
-        <li>
-           <img class="background" src="media/event_img.png">
-           <a href="accountsettings.php"><span class="name"><?php echo $organisation_name . " - " . $username ?></span></a>
-       </li>
-        <li><a href="home.php"><i class="material-icons">home</i>Home</a></li>
+      <ul id="nav-mobile" class="side-nav">
+        <li><img class="background" src="media/event_img.png"></li>
+        <li><a href="home.php"></i>Home</a></li>
         <li><a href="new_event.php">New Event</a></li>
         <li><a href="lsp.php">Find Events</a></li>
         <li><div class="divider"></div></li>
-        <li><i class="material-icons">lock_open</i><a href="login.php?status=loggout">Logout</a></li>
+        <li>
+          <a href="accountsettings.php"><span class="name">My Account - <?php echo $username ?></span></a>
+        </li>
+        <li>
+          <a href="login.php?status=loggout">Logout</a>
+        </li>
       </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
