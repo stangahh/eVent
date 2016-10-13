74f574bfea7c6d767d59586dd0b4fafcf21d203f
Nyx- <mnhunter@outlook.com>
2016-10-13 22:12:03 +1000

    Added function to view events you're going to

--- classes/Membership.php
+++ classes/Membership.php
@@ -841,7 +841,27 @@
 			mysqli_close($connection);
 		}
 		
-		
+
+		function get_events_going_to($userid) {
+			$events_going = array();
+
+			$connection = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_NAME) OR die("Database Connection Error: " . mysqli_connect_error());
+			$query = "SELECT going_event_id FROM going WHERE going_user_id = '" . $userid . "'";
+
+			$response = mysqli_query($connection, $query);
+
+			if($response){
+				while($row = mysqli_fetch_array($response)){
+					array_push($events_going, $row['going_event_id']);
+				}
+			} else {
+				echo "Failed to retrieve events information. Are you attending any events? :)";
+			}
+
+			mysqli_close($connection);
+
+			return $events_going;
+		}
 		
 	}
 
